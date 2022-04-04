2899ca894ad7ba5e322d5e4c2f6283e7e4897dfb
svenrisse <svenrisse0@gmail.com>
2022-04-04 17:27:22 +0200

    Added function to create n x n grid

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.createElement("div");
+container.classList.add("container");
+document.body.appendChild(container);
+
+
+function createGrid(rows, cols) {
+    container.style.setProperty("--grid-rows",rows);
+    container.style.setProperty("--grid-cols",cols);
+
+    for (let i =0; i<rows*cols;i++) {
+        let div = document.createElement("div");
+        div.classList.add("square");
+        container.appendChild(div);
+    }
+}
+
+createGrid(16,16);
