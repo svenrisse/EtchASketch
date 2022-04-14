9657253209daf7f558f27f775855bd4aea729713
svenrisse <svenrisse0@gmail.com>
2022-04-14 19:27:49 +0200

    Added reset button

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+/* create container for divs */
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
+/* create button to reset board */
+const resetButton = document.createElement("button");
+resetButton.classList.add("resetButton");
+resetButton.textContent = "Reset";
+document.body.appendChild(resetButton);
 
+resetButton.addEventListener("click", event => {
+    let number = prompt("Please enter the size of the grid in a singular number below 100");
+    if (number <= 100) {
+        createGrid(number, number);
+        document.querySelectorAll(".square").forEach(item => {
+        item.style.background = "antiquewhite";
+    })
+    } else {
+        alert("You entered a number above 100, please reset again");
+    }
+    
+})
+
+/* function to create n amout of divs */
 function createGrid(rows, cols) {
     container.style.setProperty("--grid-rows",rows);
     container.style.setProperty("--grid-cols",cols);
@@ -12,13 +32,17 @@ function createGrid(rows, cols) {
         div.classList.add("square");
         container.appendChild(div);
     }
+
+    document.querySelectorAll(".square").forEach(item => {
+        item.addEventListener("mouseover", event => {
+            item.style.background = "blue";
+        })
+    })
+
 }
 createGrid(16,16);
 
-document.querySelectorAll(".square").forEach(item => {
-    item.addEventListener("click", event => {
-        item.style.background = "blue";
-    })
-})
+/* event on hover */ 
+
 
 
